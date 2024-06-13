b36afc3fcf5efec85a7c4e3257117633bf032b81
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-06-13 07:06:09 +0100

    include Slider control in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
 import MyResponsiveAreaBump from "./Areabump";
-import { data } from "./data";
+import { data as InitialData } from "./data";
+import React, { useState } from "react";
+import SliderControl from "./SliderControl";
 
 function App() {
+  const [data, setData] = useState(InitialData);
+
+  const handleSliderChange = ({ seriesIndex, pointIndex, value }) => {
+    const newData = [...data];
+    newData[seriesIndex].data[pointIndex].y = value;
+    setData(newData);
+  };
+
   return (
-    <div className="App" style={{ height: "80vh" }}>
-      <MyResponsiveAreaBump data={data} />
+    <div className="App" style={{ height: "80vh", display: "flex" }}>
+      <div style={{ flex: 1 }}>
+        <MyResponsiveAreaBump data={data} />
+      </div>
+      <div style={{ flex: 1 }}>
+        <SliderControl data={data} onChange={handleSliderChange} />
+      </div>
     </div>
   );
 }
