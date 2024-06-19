e2b957d4b307fd8646d01e0fe3c04f469c9690ee
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-06-19 06:47:58 +0100

    added title to controls

--- src/SliderControl.js
+++ src/SliderControl.js
@@ -13,51 +13,56 @@ const SliderControl = ({ data, onChange }) => {
   };
 
   return (
-    <Box sx={{ width: "100%", maxWidth: 400, margin: "auto" }}>
-      {data.map((series) => (
-        <Accordion
-          key={series.id}
-          sx={{
-            marginBottom: 2,
-            borderLeft: "0.5px solid #d3d3d3",
-            borderRight: "0.5px solid #d3d3d3",
-            borderTop: "0.5px solid #d3d3d3",
-          }}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls={`${series.id}-content`}
-            id={`${series.id}-header`}
+    <div>
+      <div className="controls-title">
+        <h2>Data Controls</h2>
+      </div>
+      <Box sx={{ width: "100%", maxWidth: 400, margin: "auto" }}>
+        {data.map((series) => (
+          <Accordion
+            key={series.id}
+            sx={{
+              marginBottom: 2,
+              borderLeft: "0.5px solid #d3d3d3",
+              borderRight: "0.5px solid #d3d3d3",
+              borderTop: "0.5px solid #d3d3d3",
+            }}
           >
-            <Typography>{series.id}</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            {series.data.map((point, pointIndex) => (
-              <Box
-                key={`${series.id}-${point.x}`}
-                sx={{ marginBottom: 2, width: "100%" }}
-              >
-                <Typography>{`Year ${point.x}: ${point.y}`}</Typography>
-                <Slider
-                  value={point.y}
-                  aria-label={`slider-${series.id}-${point.x}`}
-                  valueLabelDisplay="auto"
-                  min={0}
-                  max={50}
-                  onChange={(event, value) =>
-                    handleChange(event, {
-                      seriesIndex: data.indexOf(series),
-                      pointIndex,
-                      value,
-                    })
-                  }
-                />
-              </Box>
-            ))}
-          </AccordionDetails>
-        </Accordion>
-      ))}
-    </Box>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`${series.id}-content`}
+              id={`${series.id}-header`}
+            >
+              <Typography>{series.id}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {series.data.map((point, pointIndex) => (
+                <Box
+                  key={`${series.id}-${point.x}`}
+                  sx={{ marginBottom: 2, width: "100%" }}
+                >
+                  <Typography>{`Year ${point.x}: ${point.y}`}</Typography>
+                  <Slider
+                    value={point.y}
+                    aria-label={`slider-${series.id}-${point.x}`}
+                    valueLabelDisplay="auto"
+                    min={0}
+                    max={50}
+                    onChange={(event, value) =>
+                      handleChange(event, {
+                        seriesIndex: data.indexOf(series),
+                        pointIndex,
+                        value,
+                      })
+                    }
+                  />
+                </Box>
+              ))}
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </Box>
+    </div>
   );
 };
 
